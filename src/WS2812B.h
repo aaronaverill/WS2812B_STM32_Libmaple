1d9d15f4ed4a04b99c989f002ea0442b990574b6
Aaron Averill <aaron@zonefivesoftware.com>
2019-08-29 20:10:36 -0400

    Obsolete code removal.

--- src/WS2812B.h
+++ src/WS2812B.h
@@ -76,8 +76,6 @@ class WS2812B {
     Color(uint8_t r, uint8_t g, uint8_t b, uint8_t w);
  // uint32_t
  //   getPixelColor(uint16_t n) const;
-  inline bool
-    canShow(void) { return (micros() - endTime) >= 300L; }
 
 	private:
 
@@ -90,13 +88,7 @@ class WS2812B {
   uint8_t
     brightness,
    *pixels,        // Holds the current LED color values, which the external API calls interact with 9 bytes per pixel + start + end empty bytes
-   *doubleBuffer,	// Holds the start of the double buffer (1 buffer for async DMA transfer and one for the API interaction.
-    rOffset,       // Index of red byte within each 3- or 4-byte pixel
-    gOffset,       // Index of green byte
-    bOffset,       // Index of blue byte
-    wOffset;       // Index of white byte (same as rOffset if no white)
-  uint32_t
-    endTime;       // Latch timing reference
+   *doubleBuffer;	// Holds the start of the double buffer (1 buffer for async DMA transfer and one for the API interaction.
 };
 
 
